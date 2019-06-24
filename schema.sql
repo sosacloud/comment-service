7d6f53feb8f999ac586098faff5cc5d6e1bf46a5
Juan Gonzalez <tehjuanandonly@gmail.com>
2019-06-24 16:29:43 -0500

    Complete Schema

--- schema.sql
+++ schema.sql
@@ -3,6 +3,16 @@ DROP DATABASE IF EXISTS SoSaCloud;
 CREATE DATABASE SoSaCloud;
 USE SoSaCloud;
 
+CREATE TABLE users(
+  user_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE songs(
+  song_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (song_id)
+);
+
 CREATE TABLE comments(
   comment_id INT NOT NULL AUTO_INCREMENT,
   user_id INT NOT NULL, song_id INT NOT NULL,
