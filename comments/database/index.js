fbc98bb0afa98ac5c8746e898938f94f3fdb85b9
Juan Gonzalez <tehjuanandonly@gmail.com>
2019-07-10 18:19:48 -0500

    Complete Response Comments Updating DB

--- comments/database/index.js
+++ comments/database/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host:'localhost',
   user: 'root',
-  password: 'Password',
+  password: 'M@trix224',
   database: 'SoSaCloud'
 });
 
