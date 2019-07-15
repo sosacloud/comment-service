ee99e7313c00586896ba329072107175069fae8f
スティーブンさん <underyourcover@icloud.com>
2019-07-14 19:40:06 -0700

    rendered initial comments service

--- comments/database/index.js
+++ comments/database/index.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host:'localhost',
   user: 'root',
-  password: 'pass',
-  database: 'SoSaCloud'
+  password: '',
+  database: 'comments'
 });
 
 connection.connect((err) => {
